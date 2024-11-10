c60302521757d60942b7c3fde0f951013741c895
duttaaindrila23 <aindriladutta392@gmail.com>
2024-11-11 01:21:29 +0530

    other helping files added

---
+++ employee-management-backend/src/env.access.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+const PORT=process.env.PORT
+const DB_URL=process.env.DB_URL
+const TOKENSECRETKEY=process.env.TOKENSECRETKEY
+const TOKENEXPIRY=process.env.TOKENEXPIRY
+
+const CLOUDNAME=process.env.CLOUDNAME
+const CLOUDAPISECRET=process.env.CLOUDAPISECRET
+const CLOUDAPIKEY=process.env.CLOUDAPIKEY
+export {PORT,DB_URL,TOKENEXPIRY,TOKENSECRETKEY,CLOUDAPIKEY,CLOUDAPISECRET,CLOUDNAME}
