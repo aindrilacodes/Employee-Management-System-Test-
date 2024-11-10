357167619d10bdb8a95fad33e1b4ebacc0d621c1
duttaaindrila23 <aindriladutta392@gmail.com>
2024-11-11 01:18:58 +0530

    Server created and started

---
+++ employee-management-backend/src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+const app = express();
+import { PORT } from "./env.access.js";
+import connectDB from "./config/db.connect.js";
+import { errorResponse } from "./helpers/ApiResponse.js";
+import ApiError from "./helpers/CustomError.js";
+import authRouter from "../src/routers/authRoute.js"
+import userRouter from "../src/routers/userRoute.js"
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+app.use("/api/auth",authRouter)
+app.use("/api/users",userRouter)
+
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500; 
+  const message = err.message || "Internal Server Error"; 
+  const Response = new errorResponse(message);
+  res.status(status).json(Response);
+});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is listen on port ${PORT}`);
+  });
+});
